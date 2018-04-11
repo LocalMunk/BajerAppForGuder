f2624d776d1a1f0a9a2354033ea5bb9b868cba3b
Martin Dalby <martindalby@live.dk>
2018-04-11 21:47:08 +0200

    initializing by the don

---
+++ app/src/main/java/com/example/localmunkapps/bajerbook/Database/DatabaseController.java
@@ -0,0 +1,21 @@
+package com.example.localmunkapps.bajerbook.Database;
+
+import com.example.localmunkapps.bajerbook.DataFiles.SystemData;
+import com.example.localmunkapps.bajerbook.DataFiles.UserData;
+
+/**
+ * Created by marti on 11-04-2018.
+ */
+
+public class DatabaseController {
+
+    public UserData getUserData(String userGUID){
+
+        return null;
+    }
+
+    public SystemData getSystemData(){
+
+        return null;
+    }
+}
