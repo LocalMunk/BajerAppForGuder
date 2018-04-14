ea7392c038359a5a5ed32a3dd456733b7c030234
frederik <bgbg>
2018-04-14 15:15:58 +0200

    Basic classes setup

---
+++ app/src/main/java/com/example/localmunkapps/bajerbook/Activities/login/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.example.localmunkapps.bajerbook.Activities.login
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.localmunkapps.bajerbook.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+
+}
