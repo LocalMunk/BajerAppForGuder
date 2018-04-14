e29e8aa064fbdf5be2cbd020488959d3730b2bfd
frederik <bgbg>
2018-04-14 15:15:08 +0200

    Basic classes setup

---
+++ app/src/main/java/com/example/localmunkapps/bajerbook/Activities/main/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.localmunkapps.bajerbook.Activities.main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.localmunkapps.bajerbook.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // test commit
+    }
+}
---
+++ app/src/main/java/com/example/localmunkapps/bajerbook/Activities/signup/SignupActivity.kt
@@ -0,0 +1,14 @@
+package com.example.localmunkapps.bajerbook.Activities.signup
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.localmunkapps.bajerbook.R
+
+
+class SignupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
